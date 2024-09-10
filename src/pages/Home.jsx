52170390a9f7bab142daac0484624b461dc6032c
Kamlesh Kandpal <kkandpal007@gmail.com>
2024-09-10 11:36:07 +0530

    json

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Sidebar from "../components/Home/Sidebar";
 import { Outlet } from "react-router-dom";
-import NextTopLoader from "nextjs-toploader";
+// import NextTopLoader from "nextjs-toploader";
 
 const Home = () => {
   return (
@@ -10,12 +10,12 @@ const Home = () => {
         <Sidebar />
       </div>
       <div className="w-5/6 border border-gray-500 rounded-xl py-4">
-        <NextTopLoader
+        {/* <NextTopLoader
           height={2}
           color="#27AE60"
           easing="cubic-bezier(.53,.21,0,1"
           showSpinner={false}
-        />
+        /> */}
         <Outlet />
       </div>
     </div>
